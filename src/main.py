be5f9ee7f28cf5a258a6a364a79d7d59bddf0145
Sungjoon Park <sungjoonpark@Sungjoonui-MacBookPro.local>
2021-01-27 12:37:10 +0900

    add exp in main.py

--- src/main.py
+++ src/main.py
@@ -85,7 +85,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--syn_path', default='../data/synthetic/', type=str, help='path to synthetic data directory')
-    parser.add_argument('--ast_path', default='../data/assistments/', type=str, help='path to real assessment data directory')
+    parser.add_argument('--ast_path', default='../data/assistments/', type=str, help='path to assistment data directory')
     
     # data : synthtic-2 / 5
     parser.add_argument('--dataset', default='ast_path', type=str, help='type ot the dataset')
@@ -97,43 +97,43 @@ def parse_args():
     # preprocessing args
     parser.add_argument('--train_max_seq_len', default=500, type=int, help='max sequence length in train batch')
     parser.add_argument('--eval_max_seq_len', default=500, type=int, help='max sequence length in dev/test batch')
-    parser.add_argument('--use_dev', default=False, type=bool, help='n')
+    parser.add_argument('--use_dev', default=False, type=bool, help='if True, use dev set')
 
     # model type
-    parser.add_argument('--model_type', default="LSTM", type=str, help='n')
+    parser.add_argument('--model_type', default="LSTM", type=str, help='model type. RNN/LSTM available')
     # model args (RNN)
-    parser.add_argument('--input_type', default='onehot', type=str, help='n')
-    parser.add_argument('--n_input_dim', default=100, type=int, help='n')
-    parser.add_argument('--embedding_dim', default=100, type=int, help='c')
-    parser.add_argument('--hidden_dim', default=200, type=int, help='c')
-    parser.add_argument('--n_layers', default=1, type=int, help='number of RNN layers')
+    parser.add_argument('--input_type', default='onehot', type=str, help='input vector type.')
+    parser.add_argument('--n_input_dim', default=100, type=int, help='number of input one-hot vector dimensions')
+    parser.add_argument('--embedding_dim', default=100, type=int, help='number of input embedding vector dimensions')
+    parser.add_argument('--hidden_dim', default=200, type=int, help='number of RNN/LSTM hidden dimensions')
+    parser.add_argument('--n_layers', default=1, type=int, help='number of RNN/LSTM layers')
 
     # train : device args
     parser.add_argument('--device', default='gpu', type=str, help='Use CPU or GPU')
     parser.add_argument('--device_id', default=0, type=int, help='GPU device id')
 
     # train : loss / opimt args
-    parser.add_argument('--loss', default='BCE', type=str, help='n')
-    parser.add_argument('--optim', default='adam', type=str, help='n')
-    parser.add_argument('--lr', default=0.001, type=float, help='n')
-    parser.add_argument('--momentum', default=0.9, type=float, help='n')
-    parser.add_argument('--n_epochs', default=10, type=int, help='n')
-    parser.add_argument('--clip_grad', default=100.0, type=float, help='n')
-    parser.add_argument('--log_steps', default=10, type=int, help='n')
+    parser.add_argument('--loss', default='BCE', type=str, help='loss type. BCE recommended.')
+    parser.add_argument('--optim', default='adam', type=str, help='optimizer type.')
+    parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
+    parser.add_argument('--momentum', default=0.9, type=float, help='momentum parameter.')
+    parser.add_argument('--n_epochs', default=10, type=int, help='total number of epochs to train.')
+    parser.add_argument('--clip_grad', default=100.0, type=float, help='gradient clipping')
+    parser.add_argument('--log_steps', default=10, type=int, help='train log steps.')
 
     # train : batch size
-    parser.add_argument('--train_batch_size', default=100, type=int, help='n')
-    parser.add_argument('--eval_batch_size', default=100, type=int, help='n')
+    parser.add_argument('--train_batch_size', default=100, type=int, help='batch size for training')
+    parser.add_argument('--eval_batch_size', default=100, type=int, help='batch size for dev/test')
 
     # train : save/load args
-    parser.add_argument('--task', default='dkt', type=str, help='n')
+    parser.add_argument('--task', default='dkt', type=str, help='dkt available.')
     parser.add_argument('--save_model', default=False, type=bool, help='whether to save model')
-    parser.add_argument('--save_dir', default="./../ckpt/", type=str, help='n')
+    parser.add_argument('--save_dir', default="./../ckpt/", type=str, help='save model path.')
     parser.add_argument('--save_epochs', default=10, type=int, help='save moodel for every n epochs')
-    parser.add_argument('--load_dir', default="./../ckpt/", type=str, help='n')
+    parser.add_argument('--load_dir', default="./../ckpt/", type=str, help='load model path')
     parser.add_argument('--load_model', default=False, type=bool, help='whether to load model')
-    parser.add_argument('--load_epoch', default=30, type=int, help='')
-    parser.add_argument('--refresh_optim', default=False, type=bool, help='n')
+    parser.add_argument('--load_epoch', default=30, type=int, help='what epoch should be loaded.')
+    parser.add_argument('--refresh_optim', default=False, type=bool, help='if loading model, do not load optimizer state.')
 
     args = parser.parse_args()
     args = check_args(args)
